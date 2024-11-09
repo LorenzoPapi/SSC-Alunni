36bb0a936ef87ec0db8cb91199cfb9d9580a9dcc
simone04 <simonecutrona04@gmail.com>
2024-11-09 20:09:45 +0100

    Aggiunte le routes nella navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {MatToolbarModule} from '@angular/material/toolbar'
 
 import {MatButtonModule} from '@angular/material/button'
 import {MatIconModule} from '@angular/material/icon'
+import { RouterModule } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +13,7 @@ import {MatExpansionModule, MatExpansionPanel} from '@angular/material/expansion
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule, CommonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { ComunicazioniComponent } from './comunicazioni/comunicazioni.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 export const routes: Routes = [
     {path: 'comunicazioni', component: ComunicazioniComponent},
+    {path: 'calendario', component: CalendarioComponent},
+    {path: 'comunita', component: CalendarioComponent},
+    {path: 'ricerca', component: CalendarioComponent},
+    {path: 'admin', component: CalendarioComponent},
 ];
