fc4cdc1352d874f950e09821aebd883a1260c882
simone04 <simonecutrona04@gmail.com>
2024-11-10 12:40:10 +0100

    Creato il Calendario

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, model } from '@angular/core';
+import {MatCardModule} from '@angular/material/card'
+import { provideNativeDateAdapter } from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker'
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDatepickerModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './calendario.component.html',
-  styleUrl: './calendario.component.scss'
+  styleUrl: './calendario.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarioComponent {
-
+  selected = model<Date | null>(null);
+  my_effect = effect(()=>{
+    console.log("somthing wrong")
+  })
 }
