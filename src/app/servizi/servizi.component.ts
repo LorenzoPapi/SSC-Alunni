31f3d989e53dfa33b2469d725e1dab08036924b2
simone04 <simonecutrona04@gmail.com>
2024-11-11 16:35:51 +0100

    Scegli servizio
    Montano perche non fai mai una minchia

--- src/app/servizi/servizi.component.ts
+++ src/app/servizi/servizi.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button' 
+import {MatIconModule} from '@angular/material/icon' 
 
 @Component({
   selector: 'app-servizi',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './servizi.component.html',
   styleUrl: './servizi.component.scss'
 })
