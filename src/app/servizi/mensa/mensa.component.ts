148210ab8e288fd2d13cd57977392961f09aa09f
simone04 <simonecutrona04@gmail.com>
2024-11-12 18:10:36 +0100

    Mensa No logica

--- src/app/servizi/mensa/mensa.component.ts
+++ src/app/servizi/mensa/mensa.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-mensa',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule,
+    MatDividerModule,
+    MatListModule,
+    CommonModule,
+    MatLabel,
+    MatButtonModule
+  ],
   templateUrl: './mensa.component.html',
   styleUrl: './mensa.component.scss'
 })
 export class MensaComponent {
+  time : "pranzo" | "cena" = "pranzo" 
+  cercano : {[key : string] : string[]} = {
+    "pranzo" : ["Tizio", "Caio", "Pippo","Palle","Tizio", "Caio", "Pippo","Palle", "Palle","Tizio", "Caio", "Pippo","Palle"],
+    "cena" : ["Mucca", "Gatto"]
+  }
+
+  liberano : {[key : string] : number}= {
+    "pranzo" : 3,
+    "cena" : 5
+  }
 
 }
